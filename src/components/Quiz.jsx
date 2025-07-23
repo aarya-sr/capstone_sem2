e66223ec0c9176d4f39916295f16c41046895f36
Aarya <aaryasrivastava2007@gmail.com>
2025-07-23 20:24:36 +0530

    .

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Quiz.css";
-import fallbackQuestions from "../constants/Fallbackdata";
+import fallbackQuestions from "../constants/Fallbackdata.js";
 
 const shuffleArray = (array) => {
   return [...array].sort(() => Math.random() - 0.5);
